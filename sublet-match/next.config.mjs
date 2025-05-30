f8815118a467dc01d1f78cdfdba6f9ba187f4dab
vithul7 <vithulravivarma@gmail.com>
2025-05-30 16:48:20 -0700

    load in env variables correctly (2)

--- sublet-match/next.config.mjs
+++ sublet-match/next.config.mjs
@@ -1,5 +1,12 @@
-import dotenv from 'dotenv'
-dotenv.config()
+import fs from 'fs'
+import path from 'path'
+
+const envFile = path.resolve(process.cwd(), '.env')
+const envVars = fs.readFileSync(envFile, 'utf-8')
+for (const line of envVars.split('\n')) {
+  const [key, value] = line.split('=')
+  if (key && value) process.env[key.trim()] = value.trim()
+}
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -15,4 +22,7 @@ const nextConfig = {
   },
 }
 
+console.log("✅ Loaded domain:", process.env.NEXT_PUBLIC_S3_BUCKET_DOMAIN)
+
 export default nextConfig
+
